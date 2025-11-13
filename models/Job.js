00e32c6d0588b3ae8d8207ab677351cd235480e4
AliQambar1 <3li100bh@gmail.com>
2025-11-13 17:07:47 +0300

    Creating Job Schema

---
+++ models/Job.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  budget: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX Design",
+      "Graphic Design",
+      "Writing & Translation",
+      "Digital Marketing",
+      "Video & Animation",
+      "Music & Audio",
+      "Business",
+      "Data & Analytics",
+      "AI & Machine Learning",
+      "Game Development",
+      "Software Testing",
+      "Cybersecurity",
+      "Other"
+    ],
+    required: true
+  },
+
+  status: {
+    type: String,
+    enum: ["Open", "Close"],
+    default: "Open",
+    required: true,
+  },
+
+  bids: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Application',
+  }],
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+
+}, { timestamps: true });
+
+const Job = mongoose.model('Job', JobSchema);
+module.exports = Job;
